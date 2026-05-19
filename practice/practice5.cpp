42c7c9289623cd114750e237af8a96ab3506d927
Sota Kumaoka <sotakumaoka7376@gmail.com>
2026-05-19 12:49:23 -0700

    practice5 done

---
+++ practice/practice5.cpp
@@ -0,0 +1,7 @@
+// The constant Function　ex) f(x) = 5
+// The Logarithmic Function.  ex) f(n) = log n 
+// The Linear Function  ex) f(n) = n
+// The N-Log-N Function ex) f(n) = n log n
+// The Quadratic Function ex) f(n) = n^2
+// The Cubic Function ex) f(n) = n^3
+// The Exponential Function f(n) = 2^n
