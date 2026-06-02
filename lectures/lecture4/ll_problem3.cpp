20524d8e52d82843434745c31fd9621f2f6be88f
Sota Kumaoka <sotakumaoka7376@gmail.com>
2026-06-02 12:51:01 -0700

    quiz対策

--- lectures/lecture4/ll_problem3.cpp
+++ lectures/lecture4/ll_problem3.cpp
@@ -1,6 +1,7 @@
 // ll_problem3.cpp
 
 #include <iostream>
+#include <cassert>
 
 using std::cout;
 using std::endl;
@@ -12,18 +13,78 @@ using std::string;
 // and the destructor clear the list.
 //
 
+class List
+{
+private:
+    struct Node
+    {
+        string data;
+        Node* next;
+    };
+    
+    Node* head = nullptr;
+public:
+    List() : head(nullptr){};
 
+    ~List(){
+        clear();
+        cout << "destructor called!\n";
+    }
+
+    bool is_empty() const {return head == nullptr;}
+
+    void print() const {
+        Node* p = head;
+        while(p != nullptr){
+            cout << p->data << " ";
+            p = p->next;
+        }
+        cout << endl;
+    }
+
+    int size() const{
+        int count = 0;
+        Node* p = head;
+        while(p != nullptr){
+            count++;
+            p = p->next;
+        }
+        return count;
+    }
+
+    void add_front(const string &s){head = new Node{s,head};}
+
+    void remove_front(){
+        if (is_empty())
+        {
+            return; // do nothing
+        }
+        else
+        { // not empty
+            assert(!is_empty());
+            Node *p = head;
+            head = head->next;
+            delete p;
+        }
+    }
+
+    void clear(){
+        while(!is_empty()){
+            remove_front();
+        }
+    }
+};
 int main()
 {
-    // List airports;
+    List airports;
 
-    // airports.add_front("BOS");
-    // airports.add_front("ATL");
-    // airports.add_front("MSP");
-    // airports.add_front("LAX");
+    airports.add_front("BOS");
+    airports.add_front("ATL");
+    airports.add_front("MSP");
+    airports.add_front("LAX");
 
-    // airports.print();
-    // cout << "Size: " << airports.size() << endl;
+    airports.print();
+    cout << "Size: " << airports.size() << endl;
 
     // // destructor called automatically: no memory leak
 } // main
