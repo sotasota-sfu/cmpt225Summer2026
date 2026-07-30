f406e88b9cb4aaca0070416c8dec2b37bc05528e
Toby Donaldson <tjd@sfu.ca>
2026-07-30 11:57:17 -0700

    updated

--- lectures/lecture30_31/fibonacci.cpp
+++ lectures/lecture30_31/fibonacci.cpp
@@ -1,6 +1,6 @@
 // fibonacci.cpp
 
-#define NDEBUG // uncomment this line to disable assertions
+// #define NDEBUG // uncomment this line to disable assertions
 #include <cassert>
 #include <ctime>
 #include <iostream>
